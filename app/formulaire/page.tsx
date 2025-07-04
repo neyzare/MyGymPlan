59af0c8ca213965475f19b37e680f8b1a4e82a05
neyzar <lucas.narguet451@gmail.com>
2025-07-04 10:38:19 +0200

    add ui animation modal

--- app/formulaire/page.tsx
+++ app/formulaire/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Label } from "@/components/ui/label";
+import { Label } from "@radix-ui/react-label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import {
@@ -9,20 +9,16 @@ import {
   IconBrandOnlyfans,
 } from "@tabler/icons-react";
 
-export default function SignupFormDemo() {
+function SignupForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted");
   };
   return (
-    <div className="shadow-input mx-auto w-full max-w-md rounded-none bg-white p-4 md:rounded-2xl md:p-8 dark:bg-black">
-      <h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200">
-        Welcome to Aceternity
+    <div className="shadow-input mx-auto w-full max-w-md rounded-none bg-white p-4 md:rounded-2xl md:p-8 dark:bg-white mt-16 border-2 border-blue-300">
+      <h2 className="text-xl font-bold text-neutral-800 dark:text-black">
+        Bienvenue sur MyGymPlan
       </h2>
-      <p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
-        Login to aceternity if you can because we don&apos;t have a login flow
-        yet
-      </p>
 
       <form className="my-8" onSubmit={handleSubmit}>
         <div className="mb-4 flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
@@ -89,7 +85,7 @@ export default function SignupFormDemo() {
           >
             <IconBrandOnlyfans className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
             <span className="text-sm text-neutral-700 dark:text-neutral-300">
-              OnlyFans
+              OnlyFans pour benoit
             </span>
             <BottomGradient />
           </button>
@@ -102,7 +98,7 @@ export default function SignupFormDemo() {
 const BottomGradient = () => {
   return (
     <>
-      <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent bg-[#1D4ED8] to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
+      <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
       <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
     </>
   );
@@ -121,3 +117,5 @@ const LabelInputContainer = ({
     </div>
   );
 };
+
+export default SignupForm
