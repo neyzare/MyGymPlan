5df29518657deb7c24d5af6b0ab23575589867c0
neyzar <lucas.narguet451@gmail.com>
2025-07-04 12:32:25 +0200

    add ui navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { AppleCardsCarousel } from "./ component/AppleCardCarousel";
 
 export default function Home() {
   return (
     <>
     <h1>MyGymPlan</h1>
+    <AppleCardsCarousel/>
     </>
   );
 }
